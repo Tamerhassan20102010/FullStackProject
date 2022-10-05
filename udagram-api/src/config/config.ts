810079da7dbc547267dfe1dea15d80209effd74d
Tamerhassan20102010 <98088404+Tamerhassan20102010@users.noreply.github.com>
2022-10-06 00:05:38 +0200

    c

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -12,7 +12,7 @@ export const config = {
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
-  // accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+  url: process.env.URL,
 
   jwt: {
     secret: process.env.JWT_SECRET,
