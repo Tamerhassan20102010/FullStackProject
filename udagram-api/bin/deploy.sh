b194017bcdede8ffa4bd5d0034f72405c32ed32e
Tamerhassan20102010 <98088404+Tamerhassan20102010@users.noreply.github.com>
2022-10-05 21:01:08 +0200

    Update deploy.sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,11 +1,11 @@
 eb init  udagram-api-div -r $AWS_DEFAULT_REGION -p node.js
 eb use  udagram-api-div
 eb deploy  udagram-api-div
-eb setenv AWS_PROFILE=$AWS_DEFAULT_PROFILE
+
 eb setenv AWS_BUCKET=$AWS_BUCKET
 eb setenv AWS_REGION=$AWS_DEFAULT_REGION
 eb setenv JWT_SECRET=$JWT_SECRET
-eb setenv PORT=$PORT
+
 eb setenv POSTGRES_DB=$POSTGRES_DB
 eb setenv POSTGRES_HOST=$POSTGRES_HOST
 eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
